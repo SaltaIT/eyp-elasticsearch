1c0649a0aacf665425eff65552ca3b7f8af57e82
Jordi Prats <jordi.prats@gmail.com>
2018-07-16 12:59:25 +0200

    config ES

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,11 @@
 class elasticsearch::config inherits elasticsearch {
 
+  file { '/etc/elasticsearch/elasticsearch.yml':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/elasticsearch.erb"),
+    backup  => '.back',
+  }
 }
