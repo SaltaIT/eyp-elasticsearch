49a20151183e524cbdab82b4473791ddce505d62
Jordi Prats <jordi.prats@gmail.com>
2018-07-16 12:57:17 +0200

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,21 @@
 class elasticsearch(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits elasticsearch::params{
+                      $manage_package        = true,
+                      $package_ensure        = 'installed',
+                      $manage_service        = true,
+                      $manage_docker_service = true,
+                      $service_ensure        = 'running',
+                      $service_enable        = true,
+                      $data                = true,
+                      $shards              = '1',
+                      $master              = true,
+                      $replicas            = '0',
+                      $nodename            = $::hostname,
+                      $clustername         = 'elasticsearchcluster',
+                      $network_host        = undef,
+                      $disable_dynamic     = true,
+                      $discovery_multicast = false,
+                      $discovery_unicast = [ '127.0.0.1:9300' ],
+                    ) inherits elasticsearch::params{
 
   class { '::elasticsearch::install': }
   -> class { '::elasticsearch::config': }
