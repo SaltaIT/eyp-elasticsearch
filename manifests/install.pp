4cbd56c6f9e6781baa723c8b00832451da5b48eb
Jordi Prats <jordi.prats@gmail.com>
2018-07-16 12:51:13 +0200

    lint

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,8 +3,8 @@ class elasticsearch::install inherits elasticsearch {
   if($elasticsearch::manage_package)
   {
     package { $elasticsearch::params::package_name:
-      ensure => $elasticsearch::package_ensure,
-      source => $elasticsearch::params::package_url,
+      ensure   => $elasticsearch::package_ensure,
+      source   => $elasticsearch::params::package_url,
       provider => $elasticsearch::params::package_provider,
     }
   }
